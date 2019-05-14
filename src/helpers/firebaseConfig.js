4433203e252a9ad8e5086bf9b251cb6f4a2d30a6
martin.rey <marto.rey2006@gmail.com>
2019-05-14 17:19:38 -0300

    Updated app id

--- src/helpers/firebaseConfig.js
+++ src/helpers/firebaseConfig.js
@@ -2,15 +2,16 @@ import firebase from 'firebase/app'
 import firebaseui from 'firebaseui'
 import 'firebase/firestore'
 
-const config = {
+const firebaseConfig = {
     apiKey: "AIzaSyB7HfgLVDZZ0sF9cihRzLeKcdQ3T1bsCRQ",
     authDomain: "hack-a-vote.firebaseapp.com",
     databaseURL: "https://hack-a-vote.firebaseio.com",
     projectId: "hack-a-vote",
     storageBucket: "hack-a-vote.appspot.com",
-    messagingSenderId: "94674440262"
-}
+    messagingSenderId: "94674440262",
+    appId: "1:94674440262:web:32c976f5f3453b9f"
+  };
 
-export const FBApp = firebase.initializeApp(config)
+export const FBApp = firebase.initializeApp(firebaseConfig)
 export const FBUIApp = new firebaseui.auth.AuthUI(firebase.auth(FBApp))
 export const FBFirestore = firebase.firestore()
